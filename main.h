fb208220538ec2333c4fcb99ea4ae94ca47ea6a7
under20techie <133390683+under20techie@users.noreply.github.com>
2023-09-22 05:59:19 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,29 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H 
+
 #define MAX_TOKENS 64
 #define BUF_SIZE 1024
+#define ENV_CAPACITY 20
+#define MAX_ALIAS_COUNT MAX_TOKENS 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 extern char **environ;
-// Enumeration for command types
+int env_count = 0;
+int alias_count = 0;
+int status;
+int env_capacity = ENV_CAPACITY;
+typedef struct
+{
+    char *alias;
+    char *command;
+} AliasEntry;
+AliasEntry aliases[MAX_ALIAS_COUNT];
+
+/* Enumeration for command types */
 typedef enum {
     CMD_UNKNOWN,
     CMD_SIMPLE,
@@ -34,53 +49,64 @@ typedef enum {
     TOKEN_CLOSE_BRACKET
 } TokenType;
 
-// Structure for simple commands
-typedef struct SimpleCommand {
-    CommandType type;
-    char *command;
-    char **arguments;
-    char *filename;
-} SimpleCommand;
-
 
 typedef struct {
     char *name;
     char *value;
 } EnvironmentVariable;
+EnvironmentVariable env_vars[ENV_CAPACITY];
 
 ssize_t my_get_line(char **lineptr, size_t *n);
 void print_my_prompt (void);
-char *my_strchr(const char *str, int ch);
-char *my_strtok(char *str, const char *delim);
-char *my_strdup(const char *str);
-int  get_token_type(const char *token);
+char *my_strchr(char *str, int ch);
+char *my_strtok(char *str, char *delim);
+char *my_strdup(char *str);
+int  get_token_type(char *token);
 void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strcmp(char *s1, char *s2);
 
-//The command-line parser
-void *parse_command(char **token, int num_tokens,  SimpleCommand *cmds[]);
+/* The command-line parser */
+void *parse_command(char **token, int num_tokens);
 void parse_cmd_line(char *cmd_line);
-
-// Updating  the complexCommand tree
-void set_simple_command(char **token, int *token_ptr, SimpleCommand *cmd);
-void set_pipe(char **token, int *token_ptr, SimpleCommand *cmd);
- void set_redirect_in(char **token, int *token_ptr, SimpleCommand *cmd);
-void set_redirect_out(char **token, int *token_ptr, SimpleCommand *cmd);
-void set_background (char **token, int *token_ptr, SimpleCommand *cmd);
-void set_logical_and (char **token, int *token_ptr, SimpleCommand *cmd);
-void set_logical_or(char **token, int *token_ptr, SimpleCommand *cmd);
-void set_command_separator(char **token, int *token_ptr, SimpleCommand *cmd);
-void set_open_bracket(char **token, int *token_ptr, SimpleCommand *cmd);
+void exec_pipe(char **token, int *token_ptr);
+ void exec_redirect_in(char **token, int *token_ptr);
+void exec_redirect_out(char **token, int *token_ptr);
+void exec_redirect_out(char **token, int *token_ptr);
+void exec_background (char **token, int *token_ptr);
+void exec_logical_and (char **token, int *token_ptr);
+void exec_logical_or(char **token, int *token_ptr);
+void exec_command_separator(char **token, int *token_ptr);
+void exec_open_bracket(char **token, int *token_ptr);
 void handle_single_line_comment(char* line);
 void handle_multi_line_comment(char* line);
-char* my_strstr(const char* string, const char* substring);
+char* my_strstr(char* string, char* substring);
 void* my_memset(void* dest, int val, size_t len);
-SimpleCommand * init_simple_command();
-void free_simple_command(SimpleCommand *cmd);
-void free_arguments(char **arguments);
 char *my_strcpy(char *to, char *from);
-int my_strlen(const char *str);
-char *
-my_strcat(char *s, char *append);
+int my_strlen(char *str);
+char *my_strcat(char *s, char *append);
+void interactive_mode (void);
+void non_interactive_mode (char *filename);
+void execute_external_command(char **command, int token);
+void add_environment_variable(char *name, char *value);
+void print_environment_variables();
+char* get_env(char *name);
+void set_environment_variable(char *name, char *value);
+void unset_environment_variable(char *name); 
+void free_environment();
+char *get_command_path(char *command);
+void add_alias(char *alias, char *command);
+void remove_alias(char *alias); 
+char *get_alias(char *alias); 
+void list_aliases();
+void free_aliases();
+void handle_builtin_command(char **cmd, int token);
+void execute_command(char **cmd, int token);
+void shell_exit(int exit_status);
+int my_atoi(char* str);
+void exec_simple_command(char **cmd, int *token);
+void search_forward(char **tokens, int start_index);
+void search_backward(char **tokens, int end_index) ;
+int check_for_special_characters(char **tokens);
+#endif /* MAIN_H_ */
 
-#endif /* _MAIN_H_ */
+
