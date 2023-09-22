58e54654fb7f0033c21df541213d9b91cb18b945
under20techie <133390683+under20techie@users.noreply.github.com>
2023-09-23 00:02:19 +0100

    Create my_strcpy.c

---
+++ my_strcpy.c
@@ -0,0 +1,25 @@
+char *my_strcpy(char *to, char *from)
+{
+	char *save = to;
+
+	for (; (*to = *from); ++from, ++to);
+	return(save);
+}
+int my_strlen(char *str)
+{
+	register char *s;
+
+	for ((s = str); *s; ++s)
+	    ;
+	    return(s - str);
+}
+char *my_strcat(char *s, char *append)
+{
+	char *save = s;
+
+	for (; *s; ++s)
+	    ;
+	while ((*s++ = *append++))
+	    ;
+	return(save);
+}
