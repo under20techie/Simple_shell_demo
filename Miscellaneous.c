185b74b70161c2e519323d4eb44b336b14ef1c39
under20techie <133390683+under20techie@users.noreply.github.com>
2023-09-23 03:30:44 +0100

    Create Miscellaneous.c

---
+++ Miscellaneous.c
@@ -0,0 +1,36 @@
+#include "main.h"
+int my_atoi(char* str)
+{
+    int res = 0, i;
+    for (i = 0; str[i] != '\0'; ++i)
+        res = res * 10 + str[i] - '0';
+        
+    return res;
+}
+
+
+void shell_exit(int exit_status)
+{
+
+    exit(exit_status);
+    /* Add any additional cleanup or termination steps if needed
+    */
+}  
+
+int check_for_special_characters(char **tokens) 
+{
+    int i;
+    for (i = 0; tokens[i] != NULL; i++) 
+    {
+        char *token = tokens[i];
+        if (my_strchr(token, '|') || my_strchr(token, ';') ||
+            my_strchr(token, '&') || my_strchr(token, '<') ||
+            my_strchr(token, '>') || my_strstr(token, "&&") ||
+            my_strstr(token, "||")) 
+            {
+            return 1; /* Special character found */
+        }
+    }
+    
+    return 0; /* No special characters found */
+}
