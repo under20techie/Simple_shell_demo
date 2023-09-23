699fd9c76b5d928fc66aa83e903bf62de2f45fd2
under20techie <133390683+under20techie@users.noreply.github.com>
2023-09-23 04:30:35 +0100

    Update execve.c

--- execve.c
+++ execve.c
@@ -90,7 +90,7 @@ void execute_external_command(char **command, int token)
      * provided function
     */
     char *command_path = get_command_path(command[token - 1]);
-    static int status = 0;
+    int status = 0;
     if (command_path) 
     {
         /** Fork a child process to execute
