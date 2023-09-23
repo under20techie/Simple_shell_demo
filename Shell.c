10b631db1a91434e7b34154c1811e736c3960310
under20techie <133390683+under20techie@users.noreply.github.com>
2023-09-23 04:07:08 +0100

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -3,6 +3,10 @@ int main (int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
+    int env_count = 0;
+    int alias_count = 0;
+    int status;
+    int env_capacity = ENV_CAPACITY;
 	
     if (!isatty(STDIN_FILENO))
         non_interactive_mode((void *) 0);
