a69116ebfb094008fb55dffa48b00dfadd2e5c29
under20techie <133390683+under20techie@users.noreply.github.com>
2023-11-12 21:30:31 +0100

    Update exec_func_2.c

--- exec_func_2.c
+++ exec_func_2.c
@@ -2,25 +2,29 @@
 /* Implement set_logical_and */
 void exec_logical_and(char **token, int *token_ptr) 
 {
-    
+    (void) token;
+    (void) token_ptr;
     
 }
 
 /* Implement set_logical_or */
 void exec_logical_or(char **token, int *token_ptr) 
 {
-    
+    (void) token;
+    (void) token_ptr;
     
 }
 
 /* Implement set_command_separator */
 void exec_command_separator(char **token, int *token_ptr) 
 {
-   
+   (void) token;
+    (void) token_ptr;
       
 }
 void exec_simple_command (char **cmd, int *token)
 {
-   
+   (void) token;
+(void) token_ptr;
 
 }
